21e08f72ddc561ce00f86e708f127bcfacf48b80
David Acosta <davidacosta25081@yahoo.com>
2019-11-12 09:44:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const record = [
   { year: "1972", result: "N/A"},
   { year: "1971", result: "N/A"},
   { year: "1970", result: "N/A"},
-  { year: "1969", result: "L"},
+  { year: "1969", result: "W"},
   { year: "1968", result: "N/A"},
   { year: "1967", result: "N/A"},
   { year: "1966", result: "W"},
@@ -64,7 +64,7 @@ function superbowlWin() {
   let obj = record.find(obj => obj.result == 'W');
   if (obj.result == 'W') {
     return obj.year }
-  else { return }
+  else { return obj }
 
 
    }
