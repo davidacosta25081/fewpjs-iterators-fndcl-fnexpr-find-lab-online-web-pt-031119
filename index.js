2ee54fef04bf577b1dc1719c0aaf6f307f784a5d
David Acosta <davidacosta25081@yahoo.com>
2019-11-12 09:42:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const record = [
   { year: "1972", result: "N/A"},
   { year: "1971", result: "N/A"},
   { year: "1970", result: "N/A"},
-  { year: "1969", result: "W"},
+  { year: "1969", result: "L"},
   { year: "1968", result: "N/A"},
   { year: "1967", result: "N/A"},
   { year: "1966", result: "L"},
