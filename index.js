e7c35002a4c2958f05d5eaa7298f669eb7916c70
David Acosta <davidacosta25081@yahoo.com>
2019-11-12 09:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function superbowlWin() {
   let obj = record.find(obj => obj.result == 'W');
   if (obj.result == 'W') {
     return obj.year }
-  else { return obj.year === 'undefined'}
+  else { return }
 
 
    }
